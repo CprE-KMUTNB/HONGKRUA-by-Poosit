c87b285984f1a14f15e42b87ff6caf395d3301fc
Devpooh <109036814+ppooh1@users.noreply.github.com>
2023-09-22 21:24:39 +0700

    test commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-'use client' 
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
 import { useEffect } from "react";
@@ -8,40 +8,8 @@ export default function Home() {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
   return (
-    <div className="d-flex justify-content-center align-items-center">
-      <button
-        type="button"
-        className="btn btn-primary"
-        data-bs-toggle="modal"
-        data-bs-target="#exampleModal"
-      >
-        Launch demo modal
-      </button>
+    <div>
 
-      <div
-        className="modal fade"
-        id="exampleModal"
-        tabIndex={-1}
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Modal title
-              </h5>
-              <button
-                type="button"
-                className="btn-close"
-                data-bs-dismiss="modal"
-                aria-label="Close"
-              ></button>
-            </div>
-            <div className="modal-body">...</div>
-          </div>
-        </div>
-      </div>
     </div>
-  );
+  )
 }
