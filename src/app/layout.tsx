3b9e7e0726994caa1832ad020ca8589bedda068e
Devpooh <109036814+ppooh1@users.noreply.github.com>
2023-09-22 21:16:24 +0700

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
+
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import "bootstrap/dist/css/bootstrap.min.css"; // Import bootstrap CSS
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -14,6 +17,7 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
